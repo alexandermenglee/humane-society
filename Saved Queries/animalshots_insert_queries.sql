506ba1c7e62d16b05483eabaee5e864109c9280b
Alexander Lee <alexandermlee10@gmail.com>
2019-07-24 11:25:35 -0500

    pulling from repo

--- Saved Queries/animalshots_insert_queries.sql
+++ Saved Queries/animalshots_insert_queries.sql
@@ -1,6 +1,8 @@
-insert into dbo.AnimalShots(AnimalId, ShotId, DateRecieved),
-values(),
-(),
-(),
-(),
-()
+insert into dbo.AnimalShots(AnimalId, ShotId, DateReceived)
+values(2, 3, '2009-10-15'),
+(3, 1, '2015-04-21'),
+(4, 5, '2007-10-28'),
+(5, 3, '2018-09-17'),
+(6, 4, '2015-02-27')
+
+select * from dbo.AnimalShots
